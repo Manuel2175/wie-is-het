dfc81baa1cc236bf7899fb7d1d8b8e65f3996c11
Manuel2175 <manuelraaijmakers7@gmail.com>
2025-06-06 11:36:26 +0200

    migrations ran

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('friends');
+            $table->string('img');
+            $table->integer('wins');
+            $table->integer('losses');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2025_06_06_093359_game.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('player1_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('player2_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('player1Choice_id')->constrained('characters')->onDelete('cascade');
+            $table->foreignId('player2Choice_id')->constrained('characters')->onDelete('cascade');
+            $table->foreignId('chat_id')->constrained('chat')->onDelete('cascade');
+            $table->boolean('turn');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
